e65db8e6ccd2aa338f714b00279ba505ef33bebf
Jon Sneyers <jon@cloudinary.com>
2021-05-25 22:29:34 +0200

    add --effort 1 and 2

--- lib/jxl/enc_frame.cc
+++ lib/jxl/enc_frame.cc
@@ -966,7 +966,7 @@ class LossyFrameEncoder {
     for (size_t i = 0; i < enc_state_->progressive_splitter.GetNumPasses();
          i++) {
       // No coefficient reordering in Falcon mode.
-      if (enc_state_->cparams.speed_tier != SpeedTier::kFalcon) {
+      if (enc_state_->cparams.speed_tier < SpeedTier::kFalcon) {
         enc_state_->used_orders[i] = ComputeUsedOrders(
             enc_state_->cparams.speed_tier, enc_state_->shared.ac_strategy,
             Rect(enc_state_->shared.raw_quant_field));
